bba90fbf462eec041f40ba52fe3fddc31fb148c2
Andrey Filipenkov <decapitator@ukr.net>
2019-04-18 17:45:16 +0300

    create empty Xcode project

---
+++ SwipeToDeleteInsidePageVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeToDeleteInsidePageVC
+//
+//  Created by Andrey Filipenkov on 18/04/2019.
+//  Copyright © 2019 kambala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
