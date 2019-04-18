31d5d4b7a3415737ddb0ad400f593c93a652fb1a
Andrey Filipenkov <decapitator@ukr.net>
2019-04-19 01:41:06 +0300

    add 3 pages with simple tables on the sides

--- SwipeToDeleteInsidePageVC/ViewController.swift
+++ SwipeToDeleteInsidePageVC/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var cellCount = 10
+    let cellIdentifier = "cell"
+    let cellText: String
+
+
+    init(_ text: String) {
+        cellText = text
+        super.init(style: .plain)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
 
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellCount;
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = cellText
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        cellCount -= 1
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
 }
-
